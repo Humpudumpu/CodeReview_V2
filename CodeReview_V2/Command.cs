b665ad6bdc86c2c868f2980a3f766e425065c9d8
Nikhil@Desktop <Nikhil@Desktop.com>
2015-04-04 18:36:27 -0400

    InProgress: Grouping items: Define Grouping items code in XAML.

---
+++ CodeReview_V2/Command.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Input;
+
+
+namespace CodeReview_V2
+{
+    public class Command : ICommand
+    {
+        #region Fields
+        private Action<object> action;
+        #endregion
+
+        public Command(Action<object> action)
+        {
+            this.action = action;
+        }
+
+        public void Execute(object parameter)
+        {
+            action(parameter);
+        }
+
+        public event EventHandler CanExecuteChanged { add { } remove { } }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+    }
+}
